765541869eba23f9dbc56e3c24b48b93f873d5f1
bibidu3475 <bibidu3475@gmail.com>
2018-02-24 22:46:32 +0100

    ajout d'un lapin

--- public/basic.js
+++ public/basic.js
@@ -3,6 +3,7 @@ document.body.appendChild(app.view);
 
 // create a new Sprite from an image path
 var bunny = PIXI.Sprite.fromImage('required/assets/basics/bunny.png')
+var lapin = PIXI.Sprite.fromImage('required/assets/basics/bunny.png')
 
 // center the sprite's anchor point
 bunny.anchor.set(0.5);
@@ -11,7 +12,12 @@ bunny.anchor.set(0.5);
 bunny.x = app.screen.width / 2;
 bunny.y = app.screen.height / 2;
 
+lapin.scale = new PIXI.Point(2,2)
+
 app.stage.addChild(bunny);
+app.stage.addChild(lapin)
+
+const vitesse = 4
 
 // Listen for animate update
 app.ticker.add(function(delta) {
@@ -19,4 +25,18 @@ app.ticker.add(function(delta) {
     // delta is 1 if running at 100% performance
     // creates frame-independent transformation
     bunny.rotation += 0.1 * delta;
+bunny.x = bunny.x + 1
+bunny.y = bunny.y-1
+
+
+lapin.x += vitesse
+lapin.y += vitesse
+
+if (lapin.x >= app.screen.width){
+    lapin.x = 0
+}
+if (lapin.y >= app.screen.height){
+    lapin.y = 0
+}
+
 });
